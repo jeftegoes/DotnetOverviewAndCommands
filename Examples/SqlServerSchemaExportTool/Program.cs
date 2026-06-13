4f0bd223c0b7fa4ce25aa05101db74013dba2a93
jeftegoes <jefte.goes.dev@outlook.com>
2026-06-12 22:48:51 -0300

    Adding new example.

---
+++ Examples/SqlServerSchemaExportTool/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SqlServerSchemaExportTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add loggin
+            // Send email
+            // Zip Path
+            var exportScripts = new ExportScripts();
+            exportScripts.CreateScriptDataBase();
+        }
+    }
+}
